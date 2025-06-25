131752c40218d30303281e1d05773829391184c8
Nikolay Didenko <147000338+Kolyaska404@users.noreply.github.com>
2025-06-26 02:04:20 +0300

    Create base FSD model

--- src/app/routes/routes.tsx
+++ src/app/routes/routes.tsx
@@ -1,8 +1,18 @@
 import { createBrowserRouter } from "react-router";
+import { Signin } from "../../pages/login";
+import { Signup } from "../../pages/register";
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        element: <div>Hello from FSD</div>
-    }
+        element: <div>Hello, you at home page!</div>
+    },
+    {
+        path: '/signin',
+        element: <Signin />
+    },
+    {
+        path: '/signup',
+        element: <Signup/>
+    },
 ])
---
+++ src/pages/register/ui.tsx
@@ -0,0 +1,7 @@
+export function Signup() {
+    return (
+        <div>
+            It's reg page
+        </div>
+    )
+}
